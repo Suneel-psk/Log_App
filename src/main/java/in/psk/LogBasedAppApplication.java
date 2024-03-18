5700882f697cd4889c496cd46dfb4ecb08b28da9
Suneel-psk <114077877+Suneel-psk@users.noreply.github.com>
2024-03-18 16:43:15 +0530

    Update LogBasedAppApplication.java

--- src/main/java/in/psk/LogBasedAppApplication.java
+++ src/main/java/in/psk/LogBasedAppApplication.java
@@ -5,14 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LogBasedAppApplication {
-m1(){
-int a-10;
-int b-29;
-}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogBasedAppApplication.class, args);
-m2(){
-System.out.println("hello");	}
+
 
 }
