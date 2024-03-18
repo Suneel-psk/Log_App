ce080dbf27b71ad58caff2475b061bf881dac0b2
Suneel <Suneel_007>
2024-03-18 16:42:37 +0530

    m1 is added

--- src/main/java/in/psk/LogBasedAppApplication.java
+++ src/main/java/in/psk/LogBasedAppApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LogBasedAppApplication {
+m1(){
+int a-10;
+int b-29;
+}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogBasedAppApplication.class, args);
